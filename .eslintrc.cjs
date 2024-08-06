d3f3fb3f75a20e96b282f5dbd88b8d18220586de
kerbethecoder <krby.cnts@gmail.com>
2024-08-06 19:57:09 +0800

    add linting files & configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+};
