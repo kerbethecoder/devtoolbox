0b4f690a298a662bdff4175784e831744e21448a
kerbethecoder <krby.cnts@gmail.com>
2024-08-16 12:52:00 +0800

    finalize home view, and base footer; add icons

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <hr class="custom-divider" />
+    <p class="mt-9 text-center text-[#1a1a1a66]">
+      © {{ new Date().getFullYear() }} &middot;
+      <span class="font-bold">@kerbethecoder</span>
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.custom-divider {
+  border: 0;
+  height: 1px;
+  background: #1a1a1a33;
+  margin: 0;
+}
+</style>
