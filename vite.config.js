d886ab5678faa51ccb57ea8b96616da8c1053be7
kerbethecoder <krby.cnts@gmail.com>
2024-08-30 21:56:24 +0800

    fix on webpage title

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/vue',
+  base: '/devtoolbox',
 });
